fa15dec68106f700e9a355a983bc9b4575b5dbd1
Umair Ashraf <umrashrf@gmail.com>
2022-08-01 11:59:42 -0400

    Increase timeout and retry on TImeout

--- ceda.py
+++ ceda.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support.wait import TimeoutException
 
 def wait_and_click(driver, xpath):
     WebDriverWait(driver, 30).until(
@@ -34,15 +34,18 @@ while not stop:
             "https://ceda.datatrium.com/fmi/webd/CEDA?homeurl=https://cedamtl.org/francisation-au-quebec/"
         )
         xpath1 = "//div[@role='dialog']"
-        WebDriverWait(driver, 120).until(
+        WebDriverWait(driver, 60 * 10).until(
             EC.presence_of_element_located((By.XPATH, xpath1))
         )
         elems = driver.find_elements(By.XPATH, xpath1)
         if len(elems) > 0:
             wait_and_click(driver, "//span[@class='v-button-caption']")
             wait_and_click(driver, "//div[text()='Francisation']")
+            time.sleep(2)
         else:
             stop = True
+    except TimeoutException as timeout_exc:
+        pass
     except:
         driver.close()
         os.system('say "Uh oh"')
